748e942abb53cab7350be9f430397a8d5dd6b03b
vadim-ggt <koval.cap.stop.j@gmail.com>
2026-02-01 01:13:15 +0300

    Initial commit: Spring Boot Auth Service

---
+++ src/test/java/com/innowise/authservice/TestAuthserviceApplication.java
@@ -0,0 +1,11 @@
+package com.innowise.authservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAuthserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(AuthserviceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
